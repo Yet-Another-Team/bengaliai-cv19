2c91aaee6584d583d1a08d5d94f746734f39ddcc
Dmitry Grechka <dmitry@grechka.family>
2020-03-11 13:29:37 +0300

    updating experiment dgrechka_3 and dgrechka_4 comparison

--- code/scripts/TrainResultsPairedTTest.R
+++ code/scripts/TrainResultsPairedTTest.R
@@ -1,8 +1,9 @@
 args <- commandArgs(trailingOnly = TRUE)
 experiment1OutPath <- args[1]
 experiment2OutPath <- args[2]
-metric_column <- args[3]
-outFile <- args[4]
+outFile <- args[3]
+maxMetricToLoadColumn <- "val_root_recall" # which metric was maximized during training, thus last checkpoint corresponds to max of this metirc
+metricToEvaluateColumn <- args[4]
 
 exp1_runs <- as.numeric(dir(experiment1OutPath))
 exp1_runs <- exp1_runs[!is.na(exp1_runs)]
@@ -13,11 +14,11 @@ exp_runs <- intersect(exp1_runs,exp2_runs)
 a = c()
 b = c()
 for(exp_run in exp_runs) {
-  df1 <- read.csv(file.path(experiment1OutPath,exp_run,'training_log.csv'))
-  metric1 <- max(df1[[metric_column]])
+  df1 <- read.csv(file.path(experiment1OutPath,exp_run,'training_log.csv'))  
+  metric1 <- df1[[metricToEvaluateColumn]][which.max(df1[[maxMetricToLoadColumn]])]
   a = c(a,metric1)
   df2 <- read.csv(file.path(experiment2OutPath,exp_run,'training_log.csv'))
-  metric2 <- max(df2[[metric_column]])
+  metric2 <-  df2[[metricToEvaluateColumn]][which.max(df2[[maxMetricToLoadColumn]])]
   b = c(b,metric2)
 }
 
@@ -28,14 +29,15 @@ Model2 <- b
 
 t_res <- t.test(Model1,Model2,paired = T)
 sink(outFile);
-print(paste0("Metric is ",metric_column))
-print("Model 1 metric values:")
+print(paste0("Metric that was maximized during training ",maxMetricToLoadColumn))
+print(paste0("Metric T-tested ",metricToEvaluateColumn))
 print(Model1)
 print("Model 2 metric values:")
 print(Model2)
 print(t_res);
 sink() 
-print(paste0("Metric is ",metric_column))
+print(paste0("Metric that was maximized during training ",maxMetricToLoadColumn))
+print(paste0("Metric T-tested ",metricToEvaluateColumn))
 print("Model 1 metric values:")
 print(Model1)
 print("Model 2 metric values:")
